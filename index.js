0cf602c6bebf29f156e9d69b6207e36200cc14d8
Leandro Ostera <me@leostera.com>
2013-11-11 15:21:23 -0300

    Expose the utils module

--- index.js
+++ index.js
@@ -2,14 +2,21 @@
  * Module dependencies.
  */
 
-var scaffolder = require('./scaffolder');
-var module = require('./module');
+var scaffolder = require('./lib/scaffolder');
+var module = require('./lib/module');
+var utils = require('./lib/utils')
 
 /*
  * Expose module.
  */
 
 module.exports = main = {};
+
+/*
+ * Expose the utils module
+ */
+ 
+main.utils = utils;
 
 /*
  * Make sure we have everything we need before we start.
